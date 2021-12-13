17aa27440ba6f6db807e0b36e28429805e86a000
hezhi <hezhi@yy.com>
2021-12-13 18:03:27 +0800

    演示ppt

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ def findImageEdge(image):
 def generateImageDataAt(path):
     image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
     if image is None:
+        print('image is None')
         return None
     
     # image = cv2.resize(image, (18, 18), interpolation=cv2.INTER_CUBIC)
@@ -75,8 +76,9 @@ def generateImageDataAt(path):
     imageGray = cv2.cvtColor(imageBlur, cv2.COLOR_BGR2GRAY)
     # print('imageGray: ', imageGray)
 
-    ## 有的图可能灰度之后全是黑色，可考虑其alpha通道（png图有RGBA四通道）
+    ## 有的图可能灰度之后全是黑色（如c9_8.png），可考虑其alpha通道（png图有RGBA四通道）
     if isAllBlackImage(imageGray):
+        print('path', path)
         imageGray = alpahChannel
         imageGray = cv2.GaussianBlur(imageGray, (3, 3), 1)
         # print('use alpha channel: ', imageGray)
@@ -113,7 +115,7 @@ def generateImageDataAt(path):
     return imageOut
 
 
-def test3():
+def app():
     imageList = list()
     imageNameList = list()
     for filename in os.listdir('./data1/'):
@@ -151,9 +153,28 @@ def test3():
         print('key: ', key)
         print(value)
         
+def test3():
+    image = cv2.imread('./data1/c9_2.png', cv2.IMREAD_UNCHANGED)
+    bChannel, gChannel, rChannel, alpahChannel = cv2.split(image)
+    imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.resize(imageGray, (18, 18), interpolation=cv2.INTER_CUBIC)
+    # print(image)
+    imageBlur = cv2.GaussianBlur(image, (3, 3), 1)
+    # print(imageBlur)
 
+    sobelx = cv2.Sobel(imageBlur, cv2.CV_64F, 1, 0, ksize=3)
+    sobelx = cv2.convertScaleAbs(sobelx)
+    sobely = cv2.Sobel(imageBlur, cv2.CV_64F, 0, 1, ksize=3)
+    sobely = cv2.convertScaleAbs(sobely)
+    imageDxy = cv2.addWeighted(sobelx, 0.5, sobely, 0.5, 0)
+    print('imageDxy: \n', imageDxy)
 
+    # imageOut = cv2.cvtColor(imageGray, cv2.COLOR_GRAY2RGB)
+    # plt.imsave('./temp/3.png', imageOut)
+
+    return
 
 if __name__ == '__main__':
+    # app()
     test3()
     # generateImageDataAt('./data1/c10_1.png')
