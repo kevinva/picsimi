b67a02fb0713eb3adf647fb7a7de1a17af363f60
hezhi <hezhi@yy.com>
2021-12-14 18:28:57 +0800

    alpha 通道

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import cv2
 import os
+import sys
+import getopt
 import numpy as np
 import matplotlib.pyplot as plt
+
+CLUSTER_COUNT = 4   ## 可调参
+KEY_WORDS = ['arrow', 'back', 'close', 'add', 'indicator']
+TEST_CASES = ['ic_room_pick_heart_guest_pop_close',
+              'hotline_exit_close_icon_pressed',
+              'chat_send_gift_dialog_close',
+              'me_ic_nav_close',
+              'me_ic_tag_close',
+              'room_emotion_back',
+              'me_ic_nav_back',
+              'icon_new_nav_back',
+              'me_ic_nav_back_white',
+              'me_glamour_backgroundIMG',
+              'onekey_loginBack']
 
 def isAllBlackImage(image):
     isBlack = True
@@ -12,6 +28,16 @@ def isAllBlackImage(image):
                 isBlack = False
                 break
     return isBlack
+
+def checkMayUseAlpha(image):
+    row, col = image.shape
+    useIt = False
+    for r in range(row):
+        for c in range(col):
+            if image[r, c] != 0:
+                useIt = True
+                break
+    return useIt
 
 def findImageEdge(image):
     row, col = image.shape
@@ -60,6 +86,27 @@ def findImageEdge(image):
             break
     return topBound, leftBound, bottomBound, rightBound
 
+def getImagesPathsWith(dirPath):
+    imagePathList = list()
+    for item in os.listdir(dirPath):
+        itemPath = dirPath + '/' + item
+        if os.path.isfile(itemPath):
+            if itemPath.endswith('.png'):
+                if itemPath.endswith('2x.png'):  # 忽略3x图片，用2x即可
+                    # print(itemPath)
+
+                    imagePathList.append(itemPath)
+        else:
+            subPathList = getImagesPathsWith(itemPath)
+            if len(subPathList) > 0:
+                imagePathList.extend(subPathList)
+
+
+    return imagePathList
+
+
+def generateStatisticsPage():
+    pass
 
 def generateImageDataAt(path):
     image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
@@ -74,11 +121,15 @@ def generateImageDataAt(path):
     ## 先做高斯模糊，使图片像素分布更加平滑，增加计算容忍度
     imageBlur = cv2.GaussianBlur(image, (3, 3), 1)
     imageGray = cv2.cvtColor(imageBlur, cv2.COLOR_BGR2GRAY)
+    if checkMayUseAlpha(alpahChannel):
+        alpha = 0.7  # 可调参
+        imageGray = np.int16((1 - alpha) * imageGray + alpha * alpahChannel)
     # print('imageGray: ', imageGray)
 
     ## 有的图可能灰度之后全是黑色（如c9_8.png），可考虑其alpha通道（png图有RGBA四通道）
+    ## 有时不得不考虑下alpha通道（如hotline_exit_close_icon_pressed@2x.png，其alpha通道才正确反映轮廓特征，反而RGB不能）
     if isAllBlackImage(imageGray):
-        print('path', path)
+        # print('path', path)
         imageGray = alpahChannel
         imageGray = cv2.GaussianBlur(imageGray, (3, 3), 1)
         # print('use alpha channel: ', imageGray)
@@ -99,14 +150,16 @@ def generateImageDataAt(path):
     # print('imageNew: ',imageNew)
 
     ## 去掉上下左右空白部分
+    ## 可调参
     topBound, leftBound, bottomBound, rightBound = findImageEdge(imageNew)
     imageNew = imageNew[topBound: bottomBound + 1, leftBound: rightBound + 1]
     # print('imageNew2: ', imageNew2)
-    imageNew = cv2.resize(imageNew, (38, 38), interpolation=cv2.INTER_CUBIC) ### 目前用（38, 38）准确度最高！！！
+
+    imageNew = cv2.resize(imageNew, (38, 38), interpolation=cv2.INTER_CUBIC) ### # 可调参！目前用（38, 38）准确度最高！！！
 
     imageOut = imageNew.reshape(-1)
     imageOut = np.float32(imageOut)
-    # print(imageOut.shape)
+    # print(imageOut)
 
     # imageOut = cv2.cvtColor(imageDxy, cv2.COLOR_GRAY2RGB)
     # plt.imshow(imageOut)
@@ -115,7 +168,55 @@ def generateImageDataAt(path):
     return imageOut
 
 
-def app():
+def app(argv):
+    inputPath = ''
+    try:
+        opts, args = getopt.getopt(argv, 'hi:', ['ifile='])
+    except getopt.GetoptError:
+        print("try 'python main.py -i <xxx.xcassets folder path>'")
+        sys.exit(2)
+
+    if len(opts) == 0:
+        print("try 'python main.py -i <xxx.xcassets folder path>'")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print("try 'python main.py -i <xxx.xcassets folder path>'")
+            sys.exit()
+        elif opt in ('-i', '--ifile'):
+            inputPath = arg
+
+    if len(inputPath) == 0:
+        print('Input path is none!')
+        sys.exit()
+
+    # imagePathList = getImagesPathsWith(inputPath)
+    # imagePathListClean = list()
+    # imageNameList = list()
+    # imageList = list()
+    # for filePath in imagePathList:
+    #     pathSegments = filePath.split('/')
+
+    #     assert len(pathSegments) > 0
+
+    #     imageName = pathSegments[-2].replace('.imageset', '')
+        
+    #     # 尝试再过滤一下
+    #     found = False
+    #     for keyWord in TEST_CASES:
+    #         if keyWord in imageName:
+    #             print(filePath)
+    #             imageNameList.append(imageName)
+    #             imagePathListClean.append(filePath)
+
+    # print('Total images: {}, names: {}'.format(len(imagePathListClean), len(imageNameList)))
+
+    # for filename in imagePathListClean:
+    #     imageData = generateImageDataAt(filePath)
+    #     if imageData is not None:
+    #         imageList.append(imageData)
+
     imageList = list()
     imageNameList = list()
     for filename in os.listdir('./data1/'):
@@ -129,16 +230,15 @@ def app():
     # print(imageArr.shape)
 
     critereia = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
-    k = 5
     _, labels, (centers) = cv2.kmeans(data=imageArr,
-                                      K=k,
+                                      K=CLUSTER_COUNT,
                                       bestLabels=None,
                                       criteria=critereia,
                                       attempts=20,
                                       flags=cv2.KMEANS_RANDOM_CENTERS
                                       )
-    # print('centers: ', centers.shape)
-    # print('label: ', labels.flatten().shape)
+    print('centers: ', centers.shape)
+    print('label: ', labels.flatten().shape)
 
     result = dict()
     for index, l in enumerate(labels.flatten()):
@@ -150,9 +250,12 @@ def app():
         groupList.append(imageNameList[index])
     
     for key, value in result.items():
+        # if len(value) > 1:
         print('key: ', key)
         print(value)
         
+
+
 def test3():
     image = cv2.imread('./data1/c9_2.png', cv2.IMREAD_UNCHANGED)
     bChannel, gChannel, rChannel, alpahChannel = cv2.split(image)
@@ -175,6 +278,6 @@ def test3():
     return
 
 if __name__ == '__main__':
-    # app()
-    test3()
-    # generateImageDataAt('./data1/c10_1.png')
+    app(sys.argv[1:])
+    # test3()
+    generateImageDataAt('./data2/hotline_exit_close_icon_pressed@2x.png')
